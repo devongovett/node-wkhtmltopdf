5a765a32fd5ca95d3fcfb10cd40639d5ee83809d
Devon Govett <devongovett@gmail.com>
2013-05-31 17:21:24 -0700

    Fix piping on linux

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function wkhtmltopdf(input, options) {
   var output = options.output;
   delete options.output;
   
-  var args = ['--quiet'];
+  var args = [wkhtmltopdf.command, '--quiet'];
   for (var key in options) {
     var val = options[key];
     key = '--' + slang.dasherize(key);
@@ -16,15 +16,21 @@ function wkhtmltopdf(input, options) {
     if (val !== false)
       args.push(key);
       
-    if (typeof val !== 'boolean')
-      args.push('"' + val + '"');
+    if (typeof val !== 'boolean') {
+      // escape and quote the value if it is a string
+      if (typeof val === 'string')
+        val = '"' + val.replace(/(["\\$`])/g, '\\$1') + '"';
+        
+      args.push(val);
+    }
   }
   
   var isUrl = /(https?|file):\/\//.test(input);
   args.push(isUrl ? input : '-'); // stdin if HTML given directly
   args.push(output || '-');       // stdout if no output file
   
-  var child = spawn(wkhtmltopdf.command, args);
+  // this nasty business prevents piping problems on linux
+  var child = spawn('/bin/sh', ['-c', args.join(' ') + ' | cat']);
   if (!isUrl)
     child.stdin.end(input);
   
