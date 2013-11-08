d02f70aa52066a9bf86173554cfb53b816db06d0
Stephen Willcock <steve@rowanbeach.com>
2013-11-08 09:32:40 +0000

    Adding windows support and the ability to pass the command path in options

--- index.js
+++ index.js
@@ -8,42 +8,63 @@ function wkhtmltopdf(input, options, callback) {
     callback = options;
     options = {};
   }
-  
+
+  // Allow the command to be passed in so we don't need it in the path
+  if(options.command) {
+    wkhtmltopdf.command = options.command;
+    delete options.command;
+  }
+
   var output = options.output;
   delete options.output;
-  
+
   var args = [wkhtmltopdf.command, '--quiet'];
   for (var key in options) {
     var val = options[key];
     key = key.length === 1 ? '-' + key : '--' + slang.dasherize(key);
-    
+
     if (val !== false)
       args.push(key);
-      
+
     if (typeof val !== 'boolean') {
       // escape and quote the value if it is a string
       if (typeof val === 'string')
         val = '"' + val.replace(/(["\\$`])/g, '\\$1') + '"';
-        
+
       args.push(val);
     }
   }
-  
+
   var isUrl = /^(https?|file):\/\//.test(input);
   args.push(isUrl ? input : '-'); // stdin if HTML given directly
   args.push(output || '-');       // stdout if no output file
-  
-  // this nasty business prevents piping problems on linux
-  var child = spawn('/bin/sh', ['-c', args.join(' ') + ' | cat']);
+
+  var child = spawnPdfGenerationProcess(args);
 
   if (callback)
     child.on('exit', callback);
 
   if (!isUrl)
     child.stdin.end(input);
-  
+
   // return stdout stream so we can pipe
   return child.stdout;
+}
+
+function spawnPdfGenerationProcess(args) {
+
+    if(process.platform === 'win32') {
+
+      var binaryName = args[0];
+      args.splice(0, 1);
+      return spawn(wkhtmltopdf.command, args);
+
+  } else {
+
+      // this nasty business prevents piping problems on linux
+      return spawn('/bin/sh', ['-c', args.join(' ') + ' | cat']);
+
+  }
 }
 
 wkhtmltopdf.command = 'wkhtmltopdf';
