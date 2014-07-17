6fbf9333418339654df2474b75f2fb3d864e3b88
Isioma Nnodum <isioma.nnodum@gmail.com>
2014-07-17 15:00:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,67 +2,66 @@ var spawn = require('child_process').spawn;
 var slang = require('slang');
 
 function wkhtmltopdf(input, options, callback) {
-		if (!options) {
-				options = {};
-		} else if (typeof options == 'function') {
-				callback = options;
-				options = {};
-		}
+    if (!options) {
+        options = {};
+    } else if (typeof options == 'function') {
+        callback = options;
+        options = {};
+    }
 
-		var output = options.output;
-		delete options.output;
+    var output = options.output;
+    delete options.output;
 
-		var args = [wkhtmltopdf.command];
-		for (var key in options) {
-				var val = options[key];
+    var args = [wkhtmltopdf.command];
+    for (var key in options) {
+        var val = options[key];
 
-				if ('toc' === key ||
-						'cover' === key ||
-						'page' === key) {
-						key = slang.dasherize(key);
-				} else {
-						key = key.length === 1 ? '-' + key : '--' + slang.dasherize(key);
-				}
+        if ('toc' === key ||
+            'cover' === key ||
+            'page' === key) {
+            key = slang.dasherize(key);
+        } else {
+            key = key.length === 1 ? '-' + key : '--' + slang.dasherize(key);
+        }
 
-				if (val !== false)
-						args.push(key);
+        if (val !== false)
+            args.push(key);
 
-				if (typeof val !== 'boolean') {
-						// escape and quote the value if it is a string
-						if (typeof val === 'string')
-								val = val.replace(/(["\\$`])/g, '\\$1');
-						if (/\s/g.test(val)) {
-								val = '"' + val + '"';
-						}
+        if (typeof val !== 'boolean') {
+            // escape and quote the value if it is a string
+            if (typeof val === 'string') {
 
-						args.push(val);
-				}
-		}
+                val = val.replace(/(["\\$`])/g, '\\$1');
+            }
 
-		var isUrl = /^(https?|file):\/\//.test(input);
-		args.push(isUrl ? input : '-'); // stdin if HTML given directly
-		args.push(output || '-'); // stdout if no output file
+            args.push(val);
+        }
+    }
 
-		if (process.platform === 'win32') {
-				console.log(args.join(' '));
-				var child = spawn(args[0], args.slice(1));
-		} else {
-				// this nasty business prevents piping problems on linux
-				var child = spawn('/bin/sh', ['-c', args.join(' ') + ' | cat']);
-		}
+    var isUrl = /^(https?|file):\/\//.test(input);
+    args.push(isUrl ? input : '-'); // stdin if HTML given directly
+    args.push(output || '-'); // stdout if no output file
 
-		if (callback)
-				child.on('exit', callback);
+    if (process.platform === 'win32') {
+        console.log(args.join(' '));
+        var child = spawn(args[0], args.slice(1));
+    } else {
+        // this nasty business prevents piping problems on linux
+        var child = spawn('/bin/sh', ['-c', args.join(' ') + ' | cat']);
+    }
 
-		if (!isUrl)
-				child.stdin.end(input);
+    if (callback)
+        child.on('exit', callback);
 
-		child.stderr.on('data', function(data) {
-				console.log(data.toString());
-		});
+    if (!isUrl)
+        child.stdin.end(input);
 
-		// return stdout stream so we can pipe
-		return child.stdout;
+    child.stderr.on('data', function(data) {
+        console.log(data.toString());
+    });
+
+    // return stdout stream so we can pipe
+    return child.stdout;
 }
 
 wkhtmltopdf.command = 'wkhtmltopdf';
