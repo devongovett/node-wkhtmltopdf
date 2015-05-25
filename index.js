dce0184dcad4a874803383fb6cf7e29a88832d9a
Loïc Mahieu <mahieuloic@gmail.com>
2015-05-25 12:38:28 +0200

    Remove all trailing whitespaces

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function quote(val) {
   // escape and quote the value if it is a string and this isn't windows
   if (typeof val === 'string' && process.platform !== 'win32')
     val = '"' + val.replace(/(["\\$`])/g, '\\$1') + '"';
-    
+
   return val;
 }
 
@@ -16,10 +16,10 @@ function wkhtmltopdf(input, options, callback) {
     callback = options;
     options = {};
   }
-  
+
   var output = options.output;
   delete options.output;
-    
+
   // make sure the special keys are last
   var extraKeys = [];
   var keys = Object.keys(options).filter(function(key) {
@@ -27,7 +27,7 @@ function wkhtmltopdf(input, options, callback) {
       extraKeys.push(key);
       return false;
     }
-    
+
     return true;
   }).concat(extraKeys);
 
@@ -45,7 +45,7 @@ function wkhtmltopdf(input, options, callback) {
     var spliceArgs = [keys.indexOf('toc')+1, 0].concat(myTocArgs);
     Array.prototype.splice.apply(keys, spliceArgs);
   }
-  
+
   var args = [wkhtmltopdf.command, '--quiet'];
   keys.forEach(function(key) {
     var val = options[key];
@@ -56,11 +56,11 @@ function wkhtmltopdf(input, options, callback) {
     if (key !== 'toc' && key !== 'cover' && key !== 'page') {
       key = key.length === 1 ? '-' + key : '--' + slang.dasherize(key);
     }
-    
+
     if (val !== false) {
       args.push(key);
     }
-      
+
     if (Array.isArray(val)) {
       val.forEach(function(valPart) {
         args.push(quote(valPart));
@@ -69,7 +69,7 @@ function wkhtmltopdf(input, options, callback) {
       args.push(quote(val));
     }
   });
-  
+
   var isUrl = /^(https?|file):\/\//.test(input);
   args.push(isUrl ? quote(input) : '-');    // stdin if HTML given directly
   args.push(output ? quote(output) : '-');  // stdout if no output file
@@ -80,12 +80,12 @@ function wkhtmltopdf(input, options, callback) {
     // this nasty business prevents piping problems on linux
     var child = spawn('/bin/sh', ['-c', args.join(' ') + ' | cat']);
   }
-  
+
   // call the callback with null error when the process exits successfully
   if (callback) {
     child.on('exit', function() { callback(null); });
   }
-    
+
   // setup error handling
   var stream = child.stdout;
   function handleError(err) {
@@ -106,28 +106,28 @@ function wkhtmltopdf(input, options, callback) {
     }
     child.removeAllListeners('exit');
     child.kill();
-    
+
     // call the callback if there is one
     if (callback) {
       callback(err);
     }
-      
+
     // if not, or there are listeners for errors, emit the error event
     if (!callback || stream.listeners('error').length > 0) {
       stream.emit('error', err);
     }
   }
-  
+
   child.once('error', handleError);
   child.stderr.once('data', function(err) {
     handleError(new Error((err || '').toString().trim()));
   });
-  
+
   // write input to stdin if it isn't a url
   if (!isUrl) {
     child.stdin.end(input);
   }
-  
+
   // return stdout stream so we can pipe
   if (callback) {
     return callback(null, stream);
