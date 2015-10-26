b0ee9e8f2ecb54ce42fb60c7ccc91fa3039faa85
Karim Nassar <karim.nassar@fireeye.com>
2015-10-26 11:31:30 -0400

    refactored tests to confirm args without generating PDFs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function quote(val) {
   // escape and quote the value if it is a string and this isn't windows
   if (typeof val === 'string' && process.platform !== 'win32')
     val = '"' + val.replace(/(["\\$`])/g, '\\$1') + '"';
-    
+
   return val;
 }
 
@@ -16,18 +16,24 @@ function wkhtmltopdf(input, options, callback) {
     callback = options;
     options = {};
   }
-  
+
   var output = options.output;
   delete options.output;
-    
+
   try {
     var args = [wkhtmltopdf.command, '--quiet'].concat(processOptions(options)).concat(processInput(input));
   }
   catch (err) {
-    emitError(err, callback, process.stderr);    
+    emitError(err, callback, process.stderr);
   }
-    
+
   args.push(output ? quote(output) : '-');  // stdout if no output file
+
+  // console.log(args);
+  return genPDF(input, args, callback);
+}
+
+function genPDF(input, args, callback) {
 
   if (process.platform === 'win32') {
     var child = spawn(args[0], args.slice(1));
@@ -35,11 +41,11 @@ function wkhtmltopdf(input, options, callback) {
     // this nasty business prevents piping problems on linux
     var child = spawn('/bin/sh', ['-c', args.join(' ') + ' | cat']);
   }
-  
+
   // call the callback with null error when the process exits successfully
   if (callback)
     child.on('exit', function() { callback(null); });
-    
+
   // setup error handling
   var stream = child.stdout;
   function handleError(err) {
@@ -47,17 +53,17 @@ function wkhtmltopdf(input, options, callback) {
     child.kill();
     emitError(err, callback, stream);
   }
-  
+
   child.once('error', handleError);
   child.stderr.once('data', function(err) {
     handleError(new Error((err || '').toString().trim()));
   });
-  
+
   // write input to stdin if it isn't a url
   if (!Array.isArray(input) && !isURL(input)) {
     child.stdin.end(input);
   }
-  
+
   // return stdout stream so we can pipe
   return stream;
 }
@@ -66,7 +72,7 @@ function emitError(err, callback, stream) {
   // call the callback if there is one
   if (callback)
     callback(err);
-    
+
   // if not, or there are listeners for errors, emit the error event
   if (!callback || stream.listeners('error').length > 0)
     stream.emit('error', err);
@@ -77,7 +83,7 @@ module.exports = wkhtmltopdf;
 
 
 function processOptions(options) {
-  
+
   // make sure the special keys are last
   var extraKeys = [];
   var keys = Object.keys(options).filter(function(key) {
@@ -85,20 +91,20 @@ function processOptions(options) {
       extraKeys.push(key);
       return false;
     }
-  
+
     return true;
   }).concat(extraKeys);
 
   var opts = [];
-  
+
   keys.forEach(function(key) {
     var val = options[key];
     if (key !== 'toc' && key !== 'cover' && key !== 'page')
       key = key.length === 1 ? '-' + key : '--' + slang.dasherize(key);
-  
+
     if (val !== false)
       opts.push(key);
-    
+
     if (typeof val !== 'boolean')
       opts.push(quote(val));
   });
@@ -121,7 +127,7 @@ function processInput(inputArgs) {
   else {
     resolvedInput.push('-'); // stdin
   }
-  
+
   return resolvedInput;
 }
 
@@ -130,9 +136,9 @@ function isURL(possibleURL) {
 }
 
 function resolveInputObject(input) {
-    
+
   var type, url, options;
-  
+
   if (typeof input == 'string') {
     if (input == 'toc') {
       type = input;
@@ -155,14 +161,14 @@ function resolveInputObject(input) {
   else if (!Array.isArray(options)) {
     throw Error("Invalid 'options' Array for page '" + url + "'");
   }
-  
+
   if (type == 'toc' && url) {
     throw Error("URL is invalid for page type 'toc' in '" + url + "'");
   }
   else if (type != 'toc' && !isURL(url)) {
     throw Error("Invalid 'url' for page: " + type + "'" + url + "'");
   }
-  
+
   if (!type) {
     type = '';
   }
@@ -173,8 +179,8 @@ function resolveInputObject(input) {
   else {
     url = quote(url);
   }
-  
+
   return [type, url].concat(options);
-    
+
 }
 
--- test/index.js
+++ test/index.js
@@ -1,6 +1,11 @@
-var wkhtmltopdf = require('../index.js');
+var rewire = require('rewire');
+var wkhtmltopdf = rewire('../index.js');
 
 describe('wkhtmltopdf', function() {
+
+  wkhtmltopdf.__set__("genPDF", function(input, args, callback) {
+    return args;
+  });
 
   describe('function', function() {
     var options = {
@@ -11,11 +16,17 @@ describe('wkhtmltopdf', function() {
       output: "./google.pdf"
     };
 
-    it('should generate a PDF from a single source and global options', function() {
-      wkhtmltopdf('http://google.com', options);
+    it('should produce a well-formed command-line invocation of wkhtmltopdf from a single source and global options', function() {
+      var args = wkhtmltopdf('http://google.com', options);
+
+      var expected = 'wkhtmltopdf --quiet --header-center "TEST PDF" --header-font-size 10 --header-spacing 5 --margin-top 15 "http://google.com" "./google.pdf"';
+
+      if (args.join(' ') != expected) {
+        throw new Error("generated args don't match expected");
+      }
     });
 
-    it('should support multiple pages with individual page options', function() {
+    it('should produce a well-formed command-line invocation of wkhtmltopdf from multiple pages with individual page options', function() {
       options.output = 'multi-page.pdf';
 
       var pages = [
@@ -23,7 +34,7 @@ describe('wkhtmltopdf', function() {
           type: 'cover',
           url: 'http://google.com'
         },
-        'toc', 
+        'toc',
         {
           url: 'https://github.com',
           options: {
@@ -34,7 +45,12 @@ describe('wkhtmltopdf', function() {
         'http://wkhtmltopdf.org/usage/wkhtmltopdf.txt'
       ];
 
-      wkhtmltopdf(pages, options);
+      var args = wkhtmltopdf(pages, options);
+      var expected = 'wkhtmltopdf --quiet --header-center "TEST PDF" --header-font-size 10 --header-spacing 5 --margin-top 15 cover "http://google.com" toc   "https://github.com" --enable-toc-back-links --page-offset -2  "http://wkhtmltopdf.org/usage/wkhtmltopdf.txt" "multi-page.pdf"';
+
+      if (args.join(' ') != expected) {
+        throw new Error("generated args don't match expected");
+      }
     });
 
   });
