b3a80bf97f0e83b7c1ca4c7731f5629ba8713310
Isioma Nnodum <innodum@pivcon.com>
2017-06-28 13:50:06 -0400

    Added spawnOptions
    Immediately removed from options so they are not passed to wkhtmltopdf.
    Passed as is as the options parameter to spawn invokation

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function wkhtmltopdf(input, options, callback) {
   }
 
   var output = options.output;
+  var spawnOptions = options.spawnOptions;
   delete options.output;
 
   // make sure the special keys are last
@@ -95,14 +96,14 @@ function wkhtmltopdf(input, options, callback) {
   }
 
   if (process.platform === 'win32') {
-    var child = spawn(args[0], args.slice(1));
+    var child = spawn(args[0], args.slice(1), spawnOptions);
   } else if (process.platform === 'darwin') {
-    var child = spawn('/bin/sh', ['-c', args.join(' ') + ' | cat ; exit ${PIPESTATUS[0]}']);
+    var child = spawn('/bin/sh', ['-c', args.join(' ') + ' | cat ; exit ${PIPESTATUS[0]}'], spawnOptions);
   } else {
     // this nasty business prevents piping problems on linux
     // The return code should be that of wkhtmltopdf and not of cat
     // http://stackoverflow.com/a/18295541/1705056
-    var child = spawn(wkhtmltopdf.shell, ['-c', args.join(' ') + ' | cat ; exit ${PIPESTATUS[0]}']);
+    var child = spawn(wkhtmltopdf.shell, ['-c', args.join(' ') + ' | cat ; exit ${PIPESTATUS[0]}'], spawnOptions);
   }
 
   var stream = child.stdout;
